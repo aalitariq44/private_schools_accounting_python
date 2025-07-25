6b50678d14522892388a14e9858f7720aa1e91a6
ali <aalmlai9000@gmail.com>
2025-07-26 01:14:34 +0300

    تعديلات

--- private_schools_accounting/ui/pages/additional_fees/additional_fees_page.py
+++ private_schools_accounting/ui/pages/additional_fees/additional_fees_page.py
@@ -82,10 +82,12 @@ class AdditionalFeesPage(QWidget):
             
             title_label = QLabel("إدارة الرسوم الإضافية")
             title_label.setObjectName("pageTitle")
+            title_label.setStyleSheet("color: black;")
             text_layout.addWidget(title_label)
             
             desc_label = QLabel("إدارة الرسوم الإضافية مثل الكتب والنشاطات والخدمات الأخرى")
             desc_label.setObjectName("pageDesc")
+            desc_label.setStyleSheet("color: black;")
             text_layout.addWidget(desc_label)
             
             header_layout.addLayout(text_layout)
